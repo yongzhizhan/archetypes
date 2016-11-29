934a0b96d4339662a91e3f279f1ed79ff488242c
zhanyongzhi <yongzhi.zhan@gmail.com>
2016-11-29 15:43:45 +0800

    first commit

---
+++ archetype-generate-webapp/src/main/resources/archetype-resources/src/main/java/com/github/yongzhizhan/spring/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.github.yongzhizhan.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("images/**").addResourceLocations("images/");
+    }
+}
